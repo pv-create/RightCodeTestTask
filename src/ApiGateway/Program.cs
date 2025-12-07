4ba5473a70df489e366e7f770c7b39cb8dea604c
pavelvilkov <vilkovpy@mi-broker.ru>
2025-12-07 17:58:22 +0700

    check Program.cs

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -4,4 +4,4 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
 app.MapReverseProxy();
-app.Run();
+app.Run();//чек
